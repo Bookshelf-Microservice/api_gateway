7990a765cc52f75747ffd9ab433a11e01e532d84
furkan-A <furkanakt21@gmail.com>
2022-06-23 15:21:16 +0300

    api gateway completed

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const proxy = require('express-http-proxy');
+const axios = require('axios').default;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
@@ -7,26 +9,90 @@ const PORT = 8000;
 
 
 app.use(cors());
-// for parsing application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use(function (req, res, next) {
     console.log("Request:", req.path);
     next();
 });
 
+
+app.use('/user', proxy('http://localhost:8001'));
+app.use('/book', proxy('http://localhost:8002'));
+
 app.listen(PORT, async () => {
-    await db.ConnectDB();
 	console.log(`Server has started on port: ${PORT}`);
 });
 
 
 
+// app.get('/user/get', (req, res) => {
+//     // userServiceProxy(req, res);
+//     axios.get('http://localhost:8001/user/get', {
+//         body: req.body
+//     })
+//     .then(function (response) {
+//         console.log('api gw user/get basarili');
+//         console.log('api gw user/get : ', response);
 
+//         // res.send(response);
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     });
+// });
 
-app.post("/getUser", async (req, res) => {
-    // console.log("/getUser : ", result);
-    // res.send(result);
+// app.post('/user/signup', (req, res) => {
+//     axios.post('http://localhost:8001/signup', {
+//         body: req.body
+//     })
+//     .then(function (response) {
+//         console.log('api gw user/signup basarili');
+//         console.log('api gw user/signup : ', response);
 
-});
+//         // res.send(response);
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     });
+// });
+
+// app.post('/user/login', (req, res) => {
+//     axios.post('http://localhost:8001/login', {
+//         body: req.body
+//     })
+//     .then(function (response) {
+//         console.log('api gw user/login basarili');
+//         console.log(response.data);
+//         res.send(response.res);
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     });
+// });
+
+// app.post('/user/logout', (req, res) => {
+//     axios.post('http://localhost:8001/logout', {
+//         body: req.body
+//     })
+//     .then(function (response) {
+//         console.log('api gw user/logout basarili');
+//         res.send(response.res);
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     });
+// });
+
+// app.post('/user/delete', (req, res) => {
+//     axios.post('http://localhost:8001/delete', {
+//         body: req.body
+//     })
+//     .then(function (response) {
+//         console.log('api gw user/delete basarili');
+//         res.send(response.res);
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     });
+// });
